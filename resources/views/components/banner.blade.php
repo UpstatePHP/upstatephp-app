d8146f4bf717d37f3a4432f92c701982142637ee
Steven Wade <stevenwadejr@gmail.com>
2018-07-06 23:31:46 -0400

    Add logo to home page

--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -2,6 +2,7 @@
 <section class="intro-section" id="home">
     <div class="intro-bg"></div>
     <div class="intro-content">
+        <img src="{!! asset('img/logo.png') !!}" class="intro-logo" />
         <h1>UpstatePHP</h1>
         <h2>UpstatePHP is a community of individuals with a passion for all things web.</h2>
     </div>
