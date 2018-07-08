50c04eaf6d70c78e378ab93b49da8cdaa19a4161
Kevin Dees <kvdees@gmail.com>
2018-07-07 20:45:27 -0400

    update mobile design

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,229 @@
-!function(e){var o={};function t(n){if(o[n])return o[n].exports;var i=o[n]={i:n,l:!1,exports:{}};return e[n].call(i.exports,i,i.exports,t),i.l=!0,i.exports}t.m=e,t.c=o,t.d=function(e,o,n){t.o(e,o)||Object.defineProperty(e,o,{configurable:!1,enumerable:!0,get:n})},t.n=function(e){var o=e&&e.__esModule?function(){return e.default}:function(){return e};return t.d(o,"a",o),o},t.o=function(e,o){return Object.prototype.hasOwnProperty.call(e,o)},t.p="/",t(t.s=0)}([function(e,o,t){t(1),e.exports=t(2)},function(e,o,t){"use strict";$(window).on("load",function(){$(".loader").fadeOut(),$("#preloder").delay(400).fadeOut("slow"),$(".isotope_items").isotope(),$(".portfolio-filter li").on("click",function(){$(".portfolio-filter li").removeClass("active"),$(this).addClass("active");var e=$(this).attr("data-filter");return $(".isotope_items").isotope({filter:e,animationOptions:{duration:750,easing:"linear",queue:!1}}),!1})}),function(e){e(".menu-list").onePageNav(),e(window).on("scroll resize",function(o){var t=e(".intro-section").height()-90;e(this).scrollTop()>t?e(".header-section").addClass("sticky"):e(".header-section").removeClass("sticky"),o.preventDefault()}),e(".nav-switch").on("click",function(o){e(".mainmenu").toggleClass("active"),e(this).toggleClass("active"),o.preventDefault()}),e(".responsive").on("click",function(o){e(".menu-list").toggleClass("nav-show"),e(".menu-list li a").on("click",function(o){e(".menu-list").removeClass("nav-show"),o.preventDefault()}),o.preventDefault()}),e(window).on("scroll",function(){if(!(e(window).scrollTop()>=200))return e(".intro-content").removeClass("hideup");e(".intro-content").addClass("hideup")}),e(".work-item").magnificPopup({type:"image",gallery:{enabled:!0},zoom:{enabled:!0,duration:300}}),e(".review-carousel").owlCarousel({dots:!0,nav:!1,loop:!0,smartSpeed:700,items:1,autoplay:!0})}(jQuery),(new WOW).init(),google.maps.event.addDomListener(window,"load",function(){var e={zoom:18,center:new google.maps.LatLng(34.8521103,-82.4000958),mapTypeId:google.maps.MapTypeId.ROADMAP,scrollwheel:!1,mapTypeControl:!1,zoomControl:!1,streetViewControl:!1,styles:[{stylers:[{hue:"#000000"},{saturation:-110},{gamma:2}]}]},o=new google.maps.Map(document.getElementById("map"),e),t=new google.maps.Marker({map:o,animation:google.maps.Animation.DROP,position:new google.maps.LatLng(34.8521103,-82.4000958)});google.maps.event.addListener(t,"click",function(){infowindow.open(o,t)})})},function(e,o){}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./resources/assets/js/app.js":
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/******************
+ @ Prefect JS
+ ********************/
+
+
+
+$(window).on('load', function () {
+    /*------------------
+        Preloder
+    --------------------*/
+    $(".loader").fadeOut();
+    $("#preloder").delay(400).fadeOut("slow");
+
+    /*------------------
+        Isotope Filter
+    --------------------*/
+    var $container = $('.isotope_items');
+    $container.isotope();
+
+    $('.portfolio-filter li').on("click", function () {
+        $(".portfolio-filter li").removeClass("active");
+        $(this).addClass("active");
+        var selector = $(this).attr('data-filter');
+        $(".isotope_items").isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+        });
+        return false;
+    });
+});
+
+(function ($) {
+
+    /*------------------
+        Site Navigation
+    --------------------*/
+    var navMenu = $('.menu-list');
+    navMenu.onePageNav();
+    $(window).on('scroll resize', function (e) {
+        var introH = $('.intro-section').height() - 90;
+        if ($(this).scrollTop() > introH) {
+            $('.header-section').addClass('sticky');
+        } else {
+            $('.header-section').removeClass('sticky');
+        }
+        e.preventDefault();
+    });
+
+    $('.nav-switch').on('click', function (event) {
+        $('.mainmenu').toggleClass('active');
+        $(this).toggleClass('active');
+        event.preventDefault();
+    });
+
+    $('.responsive').on('click', function (event) {
+        $('.menu-list').toggleClass('nav-show');
+        $('.menu-list li a').on('click', function (event) {
+            $('.menu-list').removeClass('nav-show');
+            event.preventDefault();
+        });
+        event.preventDefault();
+    });
+
+    /*------------------
+        Intro Title
+    --------------------*/
+    $(window).on('scroll', function () {
+        if ($(window).scrollTop() >= 200) {
+            $(".intro-content").addClass("hideup");
+        } else {
+            return $(".intro-content").removeClass("hideup");
+        }
+    });
+
+    /*------------------
+        Magnific Popup
+    --------------------*/
+    $('.work-item').magnificPopup({
+        type: 'image',
+        gallery: { enabled: true },
+        zoom: { enabled: true, duration: 300 }
+    });
+
+    /*------------------
+        Owl Carousel
+    --------------------*/
+    $('.review-carousel').owlCarousel({
+        dots: true,
+        nav: false,
+        loop: true,
+        smartSpeed: 700,
+        items: 1,
+        autoplay: true
+    });
+})(jQuery);
+
+/*------------------
+    WOW JS
+--------------------*/
+new WOW().init();
+
+/*------------------
+    GOOGLE MAP
+--------------------*/
+function initialize() {
+    var myOptions = {
+        zoom: 18,
+        center: new google.maps.LatLng(34.8521103, -82.4000958), //change the coordinates
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        scrollwheel: false,
+        mapTypeControl: false,
+        zoomControl: false,
+        streetViewControl: false,
+        styles: [{
+            "stylers": [{
+                "hue": "#000000"
+            }, {
+                saturation: -110
+            }, {
+                gamma: 2
+            }]
+        }]
+    };
+    var map = new google.maps.Map(document.getElementById("map"), myOptions);
+    var marker = new google.maps.Marker({
+        map: map,
+        animation: google.maps.Animation.DROP,
+        position: new google.maps.LatLng(34.8521103, -82.4000958) //change the coordinates
+    });
+    google.maps.event.addListener(marker, "click", function () {
+        infowindow.open(map, marker);
+    });
+}
+google.maps.event.addDomListener(window, 'load', initialize);
+
+/***/ }),
+
+/***/ "./resources/assets/sass/app.scss":
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ 0:
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__("./resources/assets/js/app.js");
+module.exports = __webpack_require__("./resources/assets/sass/app.scss");
+
+
+/***/ })
+
+/******/ });
